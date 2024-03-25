b61e2deeb2c222b116dd4ab3d75700cbae4bd00b
Henrique Patricio <henrique_ps_silva@estudante.sesisenai.org.br>
2024-03-25 17:11:08 -0300

    Subindo o conteudo de introdução a javascript do dia 25/03/2024

--- Lógica-Programação/2024.03.25/exercicio.js
+++ Lógica-Programação/2024.03.25/exercicio.js
@@ -1,3 +1,12 @@
+/**
+    Passo a passo para criar o algoritmo
+
+    1º ler o nome
+    2º ler o sobre nome
+    3º criar o nome completo juntando nome e sobrenome
+    4º mostrar o resultado
+ */
+
 let nome = prompt("Qual é seu nome? ")
 let sobrenome = prompt("Qual é seu sobrenome? ")
 let nomeCompleto = nome + " " + sobrenome
