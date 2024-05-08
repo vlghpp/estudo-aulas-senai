18ef521b9af3c19bdc3f3e30920c7bff208deb86
Henrique Patricio <henriquepworking@gmail.com>
2024-05-08 17:10:08 -0300

    Subindo conteúdo do dia 08/05/2024 - Subindo exemplos de FOR in, FOR of, Matrizes e Jogo da velha

--- Codifição-BackEnd/2024.05.08/Jogo da Velha/index.js
+++ Codifição-BackEnd/2024.05.08/Jogo da Velha/index.js
@@ -1,5 +1,5 @@
-let botao
 let campo = new Array(3)
+let botao
 for(let i = 0; i < campo.length; i++){
     campo[i] = new Array(3)
 }
@@ -8,10 +8,14 @@ for(let i = 0; i < campo.length; i++){
     for(let j = 0; j < campo[i].length; j++){
         botao = document.createElement('button')
         botao.setAttribute('type', 'button')
-        botao.setAttribute('id', 'bt' + i + "" + j)
-        botao.setAttribute('class', 'btjogo' + i)
-        botao.setAttribute('onclick', '')
+        botao.setAttribute('id', `botao${i}${j}`)
+        botao.setAttribute('class', `linha${i}`)
+        botao.addEventListener('click', eventBotao)
         botao.append(document.createTextNode(""))
         document.body.append(botao)
     }
+}
+
+function eventBotao(){
+
 }
