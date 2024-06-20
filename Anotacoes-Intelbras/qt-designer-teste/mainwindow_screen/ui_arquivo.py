14904041869462f71fca69611a9a7092210b96ac
Henrique Patricio <henriquepworking@gmail.com>
2024-06-20 10:32:57 -0300

    update: improve layout mainwindow for alignment of buttons

--- Anotacoes-Intelbras/qt-designer-teste/mainwindow_screen/ui_arquivo.py
+++ Anotacoes-Intelbras/qt-designer-teste/mainwindow_screen/ui_arquivo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'arquivoZUdJyw.ui'
+## Form generated from reading UI file 'arquivohJyGag.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
@@ -16,7 +16,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
-    QPushButton, QSizePolicy, QWidget)
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -45,24 +45,35 @@ class Ui_MainWindow(object):
         self.label.setFont(font1)
         self.label.setStyleSheet(u"background-color: #007A39; color: white;")
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.horizontalLayoutWidget = QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
-        self.horizontalLayoutWidget.setGeometry(QRect(20, 80, 761, 80))
-        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.button_model_a = QPushButton(self.horizontalLayoutWidget)
-        self.button_model_a.setObjectName(u"button_model_a")
-        self.button_model_a.setMaximumSize(QSize(400, 75))
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 10, 121, 61))
         font2 = QFont()
         font2.setPointSize(12)
         font2.setBold(True)
+        self.label_2.setFont(font2)
+        self.label_2.setStyleSheet(u"background-color: #00A336; color: white; border: 1px solid grey;")
+        self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.widget_4 = QWidget(self.centralwidget)
+        self.widget_4.setObjectName(u"widget_4")
+        self.widget_4.setGeometry(QRect(0, 60, 801, 401))
+        self.verticalLayout = QVBoxLayout(self.widget_4)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.widget = QWidget(self.widget_4)
+        self.widget.setObjectName(u"widget")
+        self.horizontalLayout_4 = QHBoxLayout(self.widget)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.button_model_a = QPushButton(self.widget)
+        self.button_model_a.setObjectName(u"button_model_a")
+        self.button_model_a.setMaximumSize(QSize(400, 75))
         self.button_model_a.setFont(font2)
         self.button_model_a.setStyleSheet(u"background-color: #008000; color: white;")
 
         self.horizontalLayout.addWidget(self.button_model_a)
 
-        self.button_model_b = QPushButton(self.horizontalLayoutWidget)
+        self.button_model_b = QPushButton(self.widget)
         self.button_model_b.setObjectName(u"button_model_b")
         self.button_model_b.setMaximumSize(QSize(400, 75))
         self.button_model_b.setFont(font2)
@@ -70,13 +81,19 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.button_model_b)
 
-        self.horizontalLayoutWidget_2 = QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_2.setObjectName(u"horizontalLayoutWidget_2")
-        self.horizontalLayoutWidget_2.setGeometry(QRect(20, 200, 761, 80))
-        self.horizontalLayout_2 = QHBoxLayout(self.horizontalLayoutWidget_2)
+
+        self.horizontalLayout_4.addLayout(self.horizontalLayout)
+
+
+        self.verticalLayout.addWidget(self.widget)
+
+        self.widget_3 = QWidget(self.widget_4)
+        self.widget_3.setObjectName(u"widget_3")
+        self.horizontalLayout_6 = QHBoxLayout(self.widget_3)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.button_model_c = QPushButton(self.horizontalLayoutWidget_2)
+        self.button_model_c = QPushButton(self.widget_3)
         self.button_model_c.setObjectName(u"button_model_c")
         self.button_model_c.setMaximumSize(QSize(400, 75))
         self.button_model_c.setFont(font2)
@@ -84,7 +101,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2.addWidget(self.button_model_c)
 
-        self.button_model_d = QPushButton(self.horizontalLayoutWidget_2)
+        self.button_model_d = QPushButton(self.widget_3)
         self.button_model_d.setObjectName(u"button_model_d")
         self.button_model_d.setMaximumSize(QSize(400, 75))
         self.button_model_d.setFont(font2)
@@ -92,13 +109,19 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2.addWidget(self.button_model_d)
 
-        self.horizontalLayoutWidget_3 = QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_3.setObjectName(u"horizontalLayoutWidget_3")
-        self.horizontalLayoutWidget_3.setGeometry(QRect(20, 330, 761, 80))
-        self.horizontalLayout_3 = QHBoxLayout(self.horizontalLayoutWidget_3)
+
+        self.horizontalLayout_6.addLayout(self.horizontalLayout_2)
+
+
+        self.verticalLayout.addWidget(self.widget_3)
+
+        self.widget_2 = QWidget(self.widget_4)
+        self.widget_2.setObjectName(u"widget_2")
+        self.horizontalLayout_5 = QHBoxLayout(self.widget_2)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.button_model_e = QPushButton(self.horizontalLayoutWidget_3)
+        self.button_model_e = QPushButton(self.widget_2)
         self.button_model_e.setObjectName(u"button_model_e")
         self.button_model_e.setMaximumSize(QSize(400, 75))
         self.button_model_e.setFont(font2)
@@ -106,7 +129,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.button_model_e)
 
-        self.button_model_f = QPushButton(self.horizontalLayoutWidget_3)
+        self.button_model_f = QPushButton(self.widget_2)
         self.button_model_f.setObjectName(u"button_model_f")
         self.button_model_f.setMaximumSize(QSize(400, 75))
         self.button_model_f.setFont(font2)
@@ -114,13 +137,16 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.button_model_f)
 
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(10, 10, 121, 61))
-        self.label_2.setFont(font2)
-        self.label_2.setStyleSheet(u"background-color: #00A336; color: white; border: 1px solid grey;")
-        self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.horizontalLayout_5.addLayout(self.horizontalLayout_3)
+
+
+        self.verticalLayout.addWidget(self.widget_2)
+
         MainWindow.setCentralWidget(self.centralwidget)
+        self.label.raise_()
+        self.widget_4.raise_()
+        self.label_2.raise_()
 
         self.retranslateUi(MainWindow)
 
@@ -130,12 +156,12 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"ESCOLHA O MODELO SWITCH PARA O TESTE", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"INTELBRAS", None))
         self.button_model_a.setText(QCoreApplication.translate("MainWindow", u"Toggle Switch", None))
         self.button_model_b.setText(QCoreApplication.translate("MainWindow", u"Lever Switch", None))
         self.button_model_c.setText(QCoreApplication.translate("MainWindow", u"Key Switch", None))
         self.button_model_d.setText(QCoreApplication.translate("MainWindow", u"Push Button Switch", None))
         self.button_model_e.setText(QCoreApplication.translate("MainWindow", u"Dual In-line Package Switch", None))
         self.button_model_f.setText(QCoreApplication.translate("MainWindow", u"Reed Switch", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"INTELBRAS", None))
     # retranslateUi
 
