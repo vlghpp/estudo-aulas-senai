a2817c5ca21690de5d45c06fc2e6bd1be17d696c
Henrique Patricio <henriquepworking@gmail.com>
2024-08-28 17:10:39 -0300

    update: add page ecommerce DC X MARVEL

---
+++ Codifição-BackEnd/2024.08.28/Marvel_X_DC/js/dados.js
@@ -0,0 +1,16 @@
+// function arq(){
+//     const input = document.getElementById('arq')
+//     const caminhoCompleto = input.value
+//     alert(caminhoCompleto)
+// }
+
+// const input = document.querySelector('#arquivo')
+// input.addEventListener('change', function(){
+//     console.log(this.files)
+// })
+
+// const fs = require('fs');
+// fs.copyFile('C:\Users\sergi\OneDrive\Área de Trabalho\MQTT.pdf', 'destination_file.js', (err) => {
+//   if (err) throw err;
+//   console.log('File copied!');
+// });
